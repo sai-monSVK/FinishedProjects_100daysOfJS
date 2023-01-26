f3551cd49a22824b868fa876fe44c73a2220b22b
Simeon Skrada <simeon.skrada@gmail.com>
2023-01-26 18:48:17 +0100

    added another solution for third task

--- --3 RandomHexColorGenerator/script.js
+++ --3 RandomHexColorGenerator/script.js
@@ -4,24 +4,24 @@ const number = document.querySelector(".number");
 const button = document.querySelector("button");
 const body = document.querySelector("body");
 
-const arrayOfColorFunctions = [
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-];
+// const arrayOfColorFunctions = [
+//   "0",
+//   "1",
+//   "2",
+//   "3",
+//   "4",
+//   "5",
+//   "6",
+//   "7",
+//   "8",
+//   "9",
+//   "a",
+//   "b",
+//   "c",
+//   "d",
+//   "e",
+//   "f",
+// ];
 let randomColorString = "#";
 
 // listener
@@ -40,12 +40,19 @@ function changeColor() {
 }
 
 function newColorFind() {
-  for (let x = 0; x < 6; x++) {
-    let index = Math.floor(Math.random() * 16);
-    let value = arrayOfColorFunctions[index];
+//  first method
+ 
+  // for (let x = 0; x < 6; x++) {
+  //   let index = Math.floor(Math.random() * 16);
+  //   let value = arrayOfColorFunctions[index];
 
-    randomColorString += value;
-    }
+  //   randomColorString += value;
+  //   }
+
+  // second method
+
+  color = Number(Math.floor(Math.random()*9999999)).toString(16).substring(0, 7);
+  randomColorString += color;
 }
 
 // rand color on load page
