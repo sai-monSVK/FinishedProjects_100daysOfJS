54fad75e38da8beca78650347c03746740f05b11
Simeon Skrada <simeon.skrada@gmail.com>
2023-05-09 18:25:41 +0200

    24_responsive_mobile_menu_part1

---
+++ 24_responsive_mobile_menu/index.js
@@ -0,0 +1,67 @@
+const links = document.querySelectorAll('.nav ul li a');
+
+for(link of links) {
+    link.addEventListener('click', smoothScroll);
+    link.addEventListener('click', makeActive);
+}
+
+function smoothScroll(e) {
+    e.preventDefault();
+
+    const href = this.getAttribute('href'); 
+    document.querySelector(href).scrollIntoView({
+        behavior: 'smooth',
+        block: 'center'
+    })
+}
+
+// sticky header
+
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('.nav');
+    nav.classList.toggle('transparent', window.scrollY <= 0);
+});
+
+// active menu switcher
+
+function makeActive(){
+    for(link of links) {
+        link.classList.remove('active');
+    }
+    this.classList.toggle('active');
+    
+};
+
+
+const scrollBtn = document.querySelector('.scroll-to-top');
+const rootElement = document.documentElement;
+
+document.addEventListener('scroll', showBtn);
+
+function showBtn() {
+    const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+    if((rootElement.scrollTop / scrollTotal) > 0.3) {
+        scrollBtn.classList.add('show');
+    } else {
+        scrollBtn.classList.remove('show');
+    }
+
+}
+
+scrollBtn.addEventListener('click', () => {
+    rootElement.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+})
+
+const hamburger = document.querySelector('.hamburger');
+const navUl = document.querySelector('.nav ul');
+
+hamburger.addEventListener('click', () => {
+    if(navUl.style.display === 'flex') {
+        navUl.style.display = '';
+    } else {
+        navUl.style.display = 'flex';
+    }
+})
